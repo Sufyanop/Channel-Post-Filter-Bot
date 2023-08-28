aa2dcc418112fd4cc8cb87fa52f8928ea5053a11
Sufyanop <142258804+Sufyanop@users.noreply.github.com>
2023-08-28 15:39:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 12124605
-API_HASH     = "5cf3577d85fd02286535ec2296934287"
-BOT_TOKEN    = "6083504683:AAEZZAePsjvcKGWOAMNTlaveNl7DD5KZLyo"
+API_ID       = 27128129
+API_HASH     = "701008edc6bc0f76abe32d63a1cb79c7"
+BOT_TOKEN    = "6083504683:6420438743:AAEbgjNy6NJIblh6D5xMZqwxe_m02wIO38c"
 SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001980857896
-ADMIN        = 6051691339
+DATABASE_URI = "mongodb+srv://Sufyan33:<Sufyan33>@cluster0.z1cgvpb.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001885526716
+ADMIN        = 6565348629
